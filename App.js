6036c32d6bad0bddde8471e27d8e4814a2f926ac
t1kyje00@students.oamk.fi <t1kyje00@students.oamk.fi>
2023-09-19 19:18:55 +0300

    Orientation lock and release

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const [screenOrientation, setScreenOrientation] = useState('portrait')
+  const [isPortrait, setIsPortrait] = useState(true)
+
+  useEffect(() => {
+    const subscription = ScreenOrientation.addOrientationChangeListener((value) => {
+      if (value.orientationInfo === ScreenOrientation.Orientation.PORTRAIT_UP) {
+        setScreenOrientation('PORTRAIT')
+        setIsPortrait(true)
+      } else if (value.orientationInfo.orientation === ScreenOrientation.Orientation.LANDSCAPE_LEFT || value.orientationInfo.orientation === ScreenOrientation.Orientation.LANDSCAPE_RIGHT) {
+        setScreenOrientation('LANDSCAPE')
+        setIsPortrait(false)
+      }
+    })
+
+    return () => {
+      ScreenOrientation.removeOrientationChangeListener(subscription)
+    }
+  }, [])
+
+  const lockToPortrait = async () => {
+    await ScreenOrientation.lockAsync( ScreenOrientation.OrientationLock.PORTRAIT )
+  }
+  const release = async() => {
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container,isPortrait ? styles.portrait : styles.landscape]}>
+      <Text>{screenOrientation}</Text>
+
+        <View style={styles.button1}>
+          <Button title="Lock to portrait" onPress={lockToPortrait}></Button>
+            <View style={styles.button2}>
+              <Button title='Release' onPress={release} />
+            </View>
+           
+        </View>
+      
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +55,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button1: {
+
+    padding: 20,
+  },
+  button2: {
+
+    padding: 10,
+  },
+  portrait: {
+    backgroundColor: '#ccc'
+  },
+  landscape: {
+  backgroundColor: '#999'
+},
 });
